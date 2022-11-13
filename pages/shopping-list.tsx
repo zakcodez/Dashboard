3d350fc929c199dda703d1b29c0826403075e870
zakcodez <zakcodez@github.com>
2022-11-13 17:33:46 +1100

    Add templates for every page

---
+++ pages/shopping-list.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navbar } from "../components";
+
+interface Props { }
+
+interface State { }
+
+class HomePage extends React.Component<Props, State> {
+	render() {
+		return (
+			<>
+				<Navbar page="shopping-list" />
+			</>
+		)
+	}
+}
+
+export default HomePage;
