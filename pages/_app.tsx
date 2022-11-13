b9300a6ff0f281275044555475dfb0e478e9aa7f
zakcodez <zakcodez@github.com>
2022-11-13 15:32:43 +1100

    Add bootstrap

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../public/styles.css";
+
+function App({ Component, pageProps }) {
+	return <Component {...pageProps} />
+}
+
+export default App;
