721f6c169a533264e127f51fbac11d5233fbc238
zakcodez <zakcodez@github.com>
2022-11-13 16:05:51 +1100

    Add CSS and Bootstrap JS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../public/styles.css";
 
+import Script from "next/script";
+
 function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Script src="/bootstrap.min.js" />
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default App;
