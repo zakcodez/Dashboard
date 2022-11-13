d9908902dff9eda3d410dfdab4858082e410f295
zakcodez <zakcodez@github.com>
2022-11-13 19:50:07 +1100

    Add weather api to weather page

--- pages/weather.tsx
+++ pages/weather.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Navbar } from "../components";
+import { Navbar, Forecast } from "../components";
+import { GetServerSidePropsContext } from "next";
+import axios from "axios";
+import { WeatherData } from "../types";
 
-interface Props { }
+interface Props {
+	data: WeatherData;
+}
 
 interface State { }
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+	const { data } = await axios<WeatherData>("/api/weather");
+	return { props: { data } }
+}
 
 class Weather extends React.Component<Props, State> {
 	render() {
 		return (
 			<>
 				<Navbar page="weather" />
+				<Forecast data={this.props.data} />
 			</>
 		)
 	}
